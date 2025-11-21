0ff74528645ff8c7eaeae7a4e20c3a1558a36e34
huynhtri998 <huynhtri998@gmail.com>
2025-11-21 18:39:56 +0700

    initialize product service

---
+++ src/main/java/com/trilabs94/ecm_product/EcmProductApplication.java
@@ -0,0 +1,13 @@
+package com.trilabs94.ecm_product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcmProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcmProductApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trilabs94/ecm_product/EcmProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trilabs94.ecm_product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcmProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
